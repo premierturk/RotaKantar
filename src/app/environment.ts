5f7feff42830be34001630ef70f012be42a8e1ce
Deniz Arda Murat <amurat@pt.local>
2023-08-16 16:23:52 +0300

    .

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,// npm run electron -> true , npm run start -> false
+    production: true,// npm run electron -> true , npm run start -> false
     apiUrl: 'http://10.210.210.35:3656/Api',
 };
