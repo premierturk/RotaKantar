c416c4bb62445b81865802d32a9c7b536fb92f65
Deniz Arda Murat <denizarda.murat@premierturk.com>
2024-12-27 10:41:59 +0300

    .

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://rotamag.ronesans.com/Api',
+  //apiUrl: 'https://rotakmo.ronesans.com/Api',
   //apiUrl: 'http://rota.premierturk.com/Api',
   //apiUrl: 'http://10.210.210.105:3656/Api',
 };
